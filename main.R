dc4bdab45ad90f7021fdb08553a725b963e71ca4
Willie Liao <will.liao932@gmail.com>
2015-05-19 13:32:36 -0700

    Create main.R

---
+++ main.R
@@ -0,0 +1,8 @@
+### SIMPLE WEIGHTED AVERAGE OF 2 BEAT THE BENCHMARK CODES
+### XGBOOST (CHRIS):  https://www.kaggle.com/c/otto-group-product-classification-challenge/forums/t/12989/code-for-leaderboard-score-of-0-45612-in-3-minutes
+### H2O DEEP LEARNING (DATAGEEK):  https://www.kaggle.com/c/otto-group-product-classification-challenge/forums/t/14134/deep-learning-h2o-0-44
+### GETS AROUND 0.41959 ON PRIVATE LEADERBOARD
+source('read_data.R')
+source('xgboost.R')
+source('h2o.R')
+source('submit.R')
